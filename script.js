b2b2fbdd015daf3fbeffb404d64529fe2db2da8e
Nitish Doddi <dnitishvvit2022@gmail.com>
2025-07-03 11:54:46 +0530

    Updated

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function onClickLikeButton() {
   const image = document.getElementById("image");
-  const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+  const result = Math.random() < 0.5 ? 'HEADS' : 'TAILS';
 
   image.style.transition = 'transform 0.6s ease';
   image.style.transform = 'rotateY(180deg)';
 
   setTimeout(function() {
-    if (result === "Heads") {
+    if (result === "HEADS") {
       image.src = "https://media.geeksforgeeks.org/wp-content/uploads/20240108115006/heads.png";
-      image.alt = "Heads";
+      image.alt = "HEADS";
     } else {
       image.src = "https://media.geeksforgeeks.org/wp-content/uploads/20240108115006/tails.png";
-      image.alt = "Tails";
+      image.alt = "TAILS";
     }
 
     image.style.transform = 'rotateY(0deg)';
